2c67272627fdfc5040716af76b6280aee6aa16c3
Fikri San Koktas <kokfik.21@robcol.k12.tr>
2021-06-18 22:51:49 +0300

    Update and rename README.md to code.java

---
+++ code.java
@@ -0,0 +1,27 @@
+public class Employee
+{
+    private String name;
+    private double salary;
+    
+    public Employee (String employeeName, double currentSalary)
+    {
+        name = employeeName;
+        salary = currentSalary;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public void raiseSalary(double byPercent)
+    {
+        salary = (salary/100) * byPercent + salary;
+    }
+}
+
